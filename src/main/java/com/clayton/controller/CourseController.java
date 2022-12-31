3b1a6be3d4f5b2c6cdb1371c18f7489e3f3aab94
ClaytonEduard <clayton_eduard@hotmail.com>
2022-12-31 14:31:50 -0300

    Metodo save criado

--- src/main/java/com/clayton/controller/CourseController.java
+++ src/main/java/com/clayton/controller/CourseController.java
@@ -4,8 +4,13 @@ import com.clayton.model.Course;
 import com.clayton.repository.CourseRepository;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +23,15 @@ public class CourseController {
   @GetMapping
   public List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  //@RequestMapping(method = RequestMethod.POST)
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED) // somente para retornar o status 201
+  public Course create(@RequestBody Course course) {
+    return courseRepository.save(course);
+    //return ResponseEntity
+    //  .status(HttpStatus.CREATED)
+    //  .body(courseRepository.save(course)); // somente para retornar o status 201
   }
 }
