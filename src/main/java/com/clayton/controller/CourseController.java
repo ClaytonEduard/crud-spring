ae48fa381137f64305e18f6b45c1d729a49563c0
ClaytonEduard <clayton_eduard@hotmail.com>
2023-01-02 11:12:44 -0300

    mapeando dados para edicao

--- src/main/java/com/clayton/controller/CourseController.java
+++ src/main/java/com/clayton/controller/CourseController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,15 @@ public class CourseController {
   @GetMapping
   public List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  // listando o curso por id
+  @GetMapping("/{id}")
+  public ResponseEntity<Course> findById(@PathVariable Long id) {
+    return courseRepository
+      .findById(id)
+      .map(record -> ResponseEntity.ok().body(record))
+      .orElse(ResponseEntity.notFound().build());
   }
 
   //@RequestMapping(method = RequestMethod.POST)
