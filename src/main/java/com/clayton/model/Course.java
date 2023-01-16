b729f643c721dc3fc7b2ed3193951d38818b4a28
ClaytonEduard <clayton_eduard@hotmail.com>
2023-01-16 11:51:23 -0300

    Inserindo anotações para validações

--- src/main/java/com/clayton/model/Course.java
+++ src/main/java/com/clayton/model/Course.java
@@ -1,13 +1,16 @@
 package com.clayton.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
@@ -18,9 +21,15 @@ public class Course {
   @JsonProperty("_id")
   private Long id;
 
-  @Column(length = 200, nullable = false)
+  @NotBlank
+  @NotNull
+  @Length(min = 5, max = 100)
+  @Column(length = 100, nullable = false)
   private String name;
 
+  @NotNull
+  @Length(max=10)
+  @Pattern(regexp = "Back-end|Front-end")
   @Column(length = 10, nullable = false)
   private String category;
 }
