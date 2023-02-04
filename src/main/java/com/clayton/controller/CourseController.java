8226311277a243bc9802bd1ebbeeeca1e9ba95bf
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-04 20:10:42 -0300

    Refatorando controler com service

--- src/main/java/com/clayton/controller/CourseController.java
+++ src/main/java/com/clayton/controller/CourseController.java
@@ -2,13 +2,11 @@ package com.clayton.controller;
 
 import com.clayton.model.Course;
 import com.clayton.repository.CourseRepository;
-
+import com.clayton.service.CourseService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-
 import java.util.List;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,26 +17,34 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor //construtor via lombok
 public class CourseController {
 
-  private final CourseRepository courseRepository;
+  private final CourseService courseService;
+
+  public CourseController(
+    CourseService courseService
+  ) {
+    this.courseService = courseService;
+  }
 
   @GetMapping
-  public List<Course> list() {
-    return courseRepository.findAll();
+  public @ResponseBody List<Course> list() {
+    return courseService.list();
   }
 
   // listando o curso por id
   @GetMapping("/{id}")
-  public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
-    return courseRepository
+  public ResponseEntity<Course> findById(
+    @PathVariable @NotNull @Positive Long id
+  ) {
+    return courseService
       .findById(id)
       .map(recordFound -> ResponseEntity.ok().body(recordFound))
       .orElse(ResponseEntity.notFound().build());
@@ -48,7 +54,7 @@ public class CourseController {
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED) // somente para retornar o status 201
   public Course create(@RequestBody @Valid Course course) {
-    return courseRepository.save(course);
+    return courseService.create(course);
     //return ResponseEntity
     //  .status(HttpStatus.CREATED)
     //  .body(courseRepository.save(course)); // somente para retornar o status 201
@@ -59,25 +65,17 @@ public class CourseController {
     @PathVariable @NotNull @Positive Long id,
     @RequestBody @Valid Course course
   ) {
-    return courseRepository
-      .findById(id)
-      .map(recordFound -> {
-        recordFound.setName(course.getName());
-        recordFound.setCategory(course.getCategory());
-        Course updated = courseRepository.save(recordFound);
-        return ResponseEntity.ok().body(updated);
-      })
+    return courseService
+      .update(id, course)
+      .map(recordFound -> ResponseEntity.ok().body(recordFound))
       .orElse(ResponseEntity.notFound().build());
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
-    return courseRepository
-      .findById(id)
-      .map(recordFound -> {
-        courseRepository.deleteById(id);
-        return ResponseEntity.noContent().<Void>build();
-      })
-      .orElse(ResponseEntity.notFound().build());
+    if (courseService.delete(id)) {
+      return ResponseEntity.noContent().<Void>build();
+    }
+    return ResponseEntity.notFound().build();
   }
 }
