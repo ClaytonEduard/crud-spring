7f390a0d2bb2056934a6d7166a1221427457b465
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-13 11:26:29 -0300

    Class Application Controlle not found

---
+++ src/main/java/com/clayton/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.clayton.controller;
+
+import com.clayton.exception.RecordNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+  @ExceptionHandler(RecordNotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String handleNotFoundException(RecordNotFoundException ex) {
+    return ex.getMessage();
+  }
+}
