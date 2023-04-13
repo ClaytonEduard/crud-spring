5a580166efacd79fa6cba34ee136a3ba6ce9a074
ClaytonEduard <clayton_eduard@hotmail.com>
2023-04-13 12:42:08 -0300

    Create class StatusConverter

---
+++ src/main/java/com/clayton/enums/converters/StatusConverter.java
@@ -0,0 +1,30 @@
+package com.clayton.enums.converters;
+
+import com.clayton.enums.Status;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.util.stream.Stream;
+
+@Converter(autoApply = true)
+public class StatusConverter implements AttributeConverter<Status, String> {
+
+  @Override
+  public String convertToDatabaseColumn(Status status) {
+    if (status == null) {
+      return null;
+    }
+    return status.getValue();
+  }
+
+  @Override
+  public Status convertToEntityAttribute(String value) {
+    if (value == null) {
+      return null;
+    }
+    return Stream
+      .of(Status.values())
+      .filter(s -> s.getValue().equals(value))
+      .findFirst()
+      .orElseThrow(IllegalArgumentException::new);
+  }
+}
