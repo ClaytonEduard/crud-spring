454f70ac6a25fdea40da7d0492d9a19e4619658b
ClaytonEduard <clayton_eduard@hotmail.com>
2023-04-13 12:41:43 -0300

    Create enum status

---
+++ src/main/java/com/clayton/enums/Status.java
@@ -0,0 +1,22 @@
+package com.clayton.enums;
+
+public enum Status {
+  ACTIVE("Ativo"),
+  INACTIVE("Inativo");
+
+  private String value;
+
+  private Status(String value) {
+    this.value = value;
+  }
+
+  public String getValue(){
+    return value;
+  }
+
+
+  @Override
+  public String toString() {
+    return value;
+  }
+}
