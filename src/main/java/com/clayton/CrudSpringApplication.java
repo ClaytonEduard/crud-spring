6ee52c0dc98a7639c0f9bd1d4324c58a64ba281b
ClaytonEduard <clayton_eduard@hotmail.com>
2023-06-02 17:17:33 -0300

    Criacao da classe lesson

--- src/main/java/com/clayton/CrudSpringApplication.java
+++ src/main/java/com/clayton/CrudSpringApplication.java
@@ -1,6 +1,7 @@
 package com.clayton;
 
 import com.clayton.model.Course;
+import com.clayton.model.Lesson;
 import com.clayton.repository.CourseRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +15,7 @@ public class CrudSpringApplication {
   public static void main(String[] args) {
     SpringApplication.run(CrudSpringApplication.class, args);
   }
+
   @Bean
   CommandLineRunner initDatabase(CourseRepository courseRepository) {
     return args -> {
@@ -21,6 +23,12 @@ public class CrudSpringApplication {
       Course c = new Course();
       c.setName("Angular com Spring");
       c.setCategory(Category.FRONT_END);
+      Lesson l = new Lesson();
+      l.setName("Introdução");
+      l.setYoutubeUrl("deu certo");
+      l.setCourse(c);
+      c.getLessons().add(l);
+
       courseRepository.save(c);
     };
   }
