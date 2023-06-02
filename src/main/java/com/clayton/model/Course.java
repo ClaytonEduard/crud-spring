481de0649f9f501ed7f808a5461f88b1ed8c4297
ClaytonEduard <clayton_eduard@hotmail.com>
2023-06-02 17:17:20 -0300

    Relacionamento muitos para um

--- src/main/java/com/clayton/model/Course.java
+++ src/main/java/com/clayton/model/Course.java
@@ -1,5 +1,8 @@
 package com.clayton.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
@@ -10,20 +13,24 @@ import com.clayton.enums.converters.CategoryConverter;
 import com.clayton.enums.converters.StatusConverter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @Entity
-@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?") // anotacao do hibernete q posibilita buscar personalizado
-@Where(clause = "status = 'Ativo'")// toda vez que for realizada uma consulta será utilizada essa condicao
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?") // anotacao do hibernete q posibilita buscar
+                                                                      // personalizado
+@Where(clause = "status = 'Ativo'") // toda vez que for realizada uma consulta será utilizada essa condicao
 public class Course {
 
   @Id
@@ -47,4 +54,12 @@ public class Course {
   @Column(length = 10, nullable = false)
   @Convert(converter = StatusConverter.class)
   private Status status = Status.ACTIVE; // sempre que criar um novo ja virar Ativo
+
+  // declaracao de 1 para muito
+  // https://vladmihalcea.com/the-best-way-to-map-a-onetomany-association-with-jpa-and-hibernate/
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course") // faz a ligacao com a classe Lesson, se for remover um
+                                                              // curso tem que remover as lissoes
+  //@JoinColumn(name = "course_id") // relacionamento 1 para muitos do lado contrario
+  private List<Lesson> lessons = new ArrayList<>();
+
 }
